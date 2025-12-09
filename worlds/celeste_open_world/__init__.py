bd28502dd9af33eb8ed261e26a0d3de37a9ac9f9
olly <dninosores@gmail.com>
2025-12-09 00:56:35 -0800

    Decrease location counter when locking checkpoint items to checkpoint locations

--- worlds/celeste_open_world/__init__.py
+++ worlds/celeste_open_world/__init__.py
@@ -140,10 +140,11 @@ class CelesteOpenWorld(World):
                 else:
                     item_pool.append(self.create_item(item_name))
                     # If checkpoints are forced to unlock themselves, assign each checkpoint item to its location
-                    # But don't decrease the location counter because locations can still be shuffled as items too
+                    # in addition to adding it to the item pool
                     if self.options.checkpoints_unlock_themselves:
                         checkpoint_loc: Location = self.get_location(item_name)
                         checkpoint_loc.place_locked_item(self.create_item(item_name))
+                        location_count -= 1
             else:
                 checkpoint_loc: Location = self.get_location(item_name)
                 checkpoint_loc.place_locked_item(self.create_item(item_name))
